4f087832e6d3c25a881362ea8ca28242562bcde4
Kristofferson Reyes <Kristofferson Reyes>
2015-10-28 14:08:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kristoffer/reyesmaps/MapsActivity.java
@@ -0,0 +1,48 @@
+package com.example.kristoffer.reyesmaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LatLng DLSU = new LatLng(14.262224, 121.043111);
+        mMap.addMarker(new MarkerOptions().position(DLSU).title("De La Salle University - Science and Technology Complex").snippet("Home of the Green Archers"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(DLSU));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
+    }
+}
